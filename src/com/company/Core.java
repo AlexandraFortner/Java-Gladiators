168425eb211c90c0cbfac80d1a084f4944bb2fe9
Alexandra <afortner@basecampcodingacademy.org>
2018-01-29 13:54:55 -0600

    Finally got two Gladiators to appear.
    Also added names to be implemented.
    
    Next is to add the attack() function.

--- src/com/company/Core.java
+++
@@ -1,8 +0,0 @@
-package com.company;
-
-public class Core {
-
-    public static void main(String[] args) {
-        // write your code here
-    }
-}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,18 @@
 package com.company;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Gladiator player1 = new Gladiator(100, 5, 10, 20);
+        Gladiator player2 = new Gladiator(100, 5, 5, 30);
+        Scanner reader = new Scanner(System.in);  // Reading from System.in
+        System.out.println("Enter Player 1's name: ");
+        String player1Name = reader.next();
+        System.out.println("Enter Player 2's name: ");
+        String player2Name = reader.next();
+        System.out.println("(" + player1Name + ")\nHealth:" + player1.health);
+        System.out.println("(" + player2Name + ")\nHealth:" + player2.health);
+        reader.close();
     }
 }
